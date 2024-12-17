a5446f83f57e5ed0e5f29559cbf957269802b49d
RASHID1104 <kleverok2020@gmail.com>
2024-12-17 17:07:32 +0400

    commit

---
+++ task3/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Input from './components/inputs'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Input/>
+      </div>
+    </>
+  )
+}
+
+export default App
