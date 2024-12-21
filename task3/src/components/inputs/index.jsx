8a21e8550734fc4c4ba0ecbd93f4be02bc1f4208
JhonneyQ <kananqadirov2005@gmail.com>
2024-12-21 13:51:37 +0400

    commit

--- task3/src/components/inputs/index.jsx
+++ task3/src/components/inputs/index.jsx
@@ -1,20 +1,32 @@
 import React, { PureComponent } from 'react'
 
+
 export class Input extends PureComponent {
   render() {
 
-    const submit = (e) =>{
-        e.preventDefault();
+    // const [studentName, setStudentName] = useState("");
+    // const [gpa, setGpa] = useState(0);
+    // const [faculty, setFaculty] = useState("");
 
+    // const submit = (e) =>{
+    //     e.preventDefault();
+    //     const student = {
+    //       studentName,
+    //       gpa,
+    //       faculty,
+    //     };
+
+    //     setStudents([...students, student]);
         
-    }
+    // }
 
 
     return (
       <form className='input' onSubmit={submit}>
         <input type='text' placeholder='name' id='name'/>
-        <input type='number' placeholder='gpa' id='gpga'/>
+        <input type='number' placeholder='gpa' id='gpa'/>
         <input type='text' placeholder='faculty' id='faculty'/>
+
       </form>
     )
   }
