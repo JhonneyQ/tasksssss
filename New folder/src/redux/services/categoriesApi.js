f6a6e9aa70fe3f524f4f09884f2776b516450755
ClassTime <classtime@code.local>
2025-01-07 19:39:10 +0400

    commit

--- New folder/src/redux/services/categoriesApi.js
+++ New folder/src/redux/services/categoriesApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const categoriesApi = createApi({
   reducerPath: "categoriesApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://northwind.vercel.app/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
     getAllCategories: builder.query({
       query: () => `categories`,
